42d318f0bbcb88bedb3ee39cbcc711f4a6859dc6
farshad68 <farshad.afshari68@gmail.com>
2020-02-12 14:19:15 +0100

    Solution Complated

--- FebruaryContestEntry/Program.cs
+++ FebruaryContestEntry/Program.cs
@@ -4,17 +4,49 @@ namespace FebruaryContestEntry
 {
 	class Program
 	{
-		const char block = '▓';
-
+		const char			block = '▓';
+		const int			Width = 30;
+		const int			Tall = 15;
+		const int			Margin = 1;
+		const ConsoleColor	MarginColor = ConsoleColor.Green;
+		const ConsoleColor	CenterColor = ConsoleColor.Yellow;
 		static void Main(string[] args)
 		{
 			// The solution implementation goes here
 
 			Console.ResetColor();
-			Console.WriteLine();
-			Console.WriteLine();
-			Console.WriteLine("Yay, a colorful shape!");
+
+			for(int row = 0; row < Tall; row++)
+			{
+				for(int column = 0; column < Width; column++)
+				{
+					setColor(row, column);
+					Console.Write(block);
+				}				
+				Console.WriteLine();
+			}
+			//Console.WriteLine("123456789012345678901234567890");
+			Console.ResetColor();			
 			Console.ReadLine();
+		}
+
+		private static void setColor(int row, int col)
+		{
+			if (isMargin(row, col))
+			{
+				Console.BackgroundColor = MarginColor;
+			}
+			else
+			{
+				Console.BackgroundColor = CenterColor;
+			}			
+		}
+
+		private static bool isMargin(int row, int col)
+		{
+			if (col < Margin || col >= Width - Margin) return true;
+			if (row < Margin || row >= Tall - Margin) return true;
+			return false;
 		}
 	}
 }
